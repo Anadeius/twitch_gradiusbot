78966557c8dad006d17ae6ebc7417410b8e9df25
gradiuscypher <gradiuscypher@gmail.com>
2013-12-05 11:58:42 -0800

    Trivia questions working properly now.

--- plugins/trivia.py
+++ plugins/trivia.py
@@ -13,14 +13,15 @@ client = pymongo.MongoClient()
 trivia_db = client.trivia_db
 status = trivia_db.status
 questions = trivia_db.questions
-
+post_id = 0
 
 def buildup(send_message_callback):
     print "This function is run at buildup of function."
     global send_message_function
     send_message_function = send_message_callback
-    s_post = {"name": "status", "asked": False, "loop": False, "loop_count": 0, "answer": "", "question": ""}
-    status.insert(s_post)
+    s_post = {"asked": False, "loop": False, "loop_count": 0, "answer": "", "question": ""}
+    global post_id
+    post_id = status.insert(s_post)
 
 
 def send_input(inp, sender, channel):
@@ -32,8 +33,8 @@ def send_input(inp, sender, channel):
 
     if len(inp.split()) == 2 and inp.split()[0] == "trivia" and perm.isMod(sender):
         send_message_function(channel,"Starting a trivia loop of " + inp.split()[1] + " questions.")
-        status.update({"name": "status"}, {"$set": {"loop": True}})
-        status.update({"name": "status"}, {"$set": {"loop_count": str(int(inp.split()[1]) - 1)}})
+        status.update({'_id': post_id}, {"$set": {"loop": True}})
+        status.update({'_id': post_id}, {"$set": {"loop_count": str(int(inp.split()[1]) - 1)}})
         trivia_question(channel)
 
 
@@ -48,25 +49,25 @@ def teardown():
 def trivia_question(channel):
     if not status.find_one()["asked"]:
         try:
-            status.update({"name": "status"}, {"$set": {"answer": "bestanswer"}})
-            status.update({"name": "status"}, {"$set": {"question": "bestquestion"}})
-            status.update({"name": "status"}, {"$set": {"asked": True}})
+            status.update({'_id': post_id}, {"$set": {"answer": "bestanswer"}})
+            status.update({'_id': post_id}, {"$set": {"question": "bestquestion"}})
+            status.update({'_id': post_id}, {"$set": {"asked": True}})
         except:
             print "Error generating question:", sys.exc_info()
 
-        send_message_function(channel, status.find_one()["question"])
+        send_message_function(channel, str(status.find_one()["question"]))
 
 
 def trivia_answer(channel, message, sender):
-    if str.lower(message) == str.lower(str(data['answer'])):
-        status.update({"name": "status"}, {"$set": {"asked": False}})
+    if str.lower(message) == str.lower(str(status.find_one()["answer"])):
+        status.update({'_id': post_id}, {"$set": {"asked": False}})
         send_message_function(channel,"You are correct " + sender + "!")
         sb.addPoints(sender, 10)
 
         if status.find_one()["loop"] and status.find_one()["loop_count"] == 0:
-            status.update({"name": "status"}, {"$set": {"loop": False}})
+            status.update({'_id': post_id}, {"$set": {"loop": False}})
             send_message_function(channel,"I'm done with my trivia spree now.")
 
         if status.find_one()["loop"] and status.find_one()["loop_count"] != 0:
-            status.update({"name": "status"}, {"$set": {"loop_count": int(status.find_one()["loop_count"])}})
+            status.update({'_id': post_id}, {"$set": {"loop_count": int(status.find_one()["loop_count"]) - 1}})
             trivia_question(channel)
