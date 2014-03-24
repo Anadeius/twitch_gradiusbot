954bef533a390ef2352c68b64000d7a513e98e5a
gradiuscypher <gradiuscypher@gmail.com>
2014-03-23 18:37:55 -0700

    Fixing trivia and adding a readme for gradiuscoin

--- plugins/games/trivia.py
+++ plugins/games/trivia.py
@@ -1,79 +1,95 @@
-import sys
-import random
-import pymongo
 from libs.permissions import Permissions
-from libs.scoreboard import Scoreboard
+from libs.temp_pickle import TempPickle
+from libs.gradiuscoins import GradiusCoins
+from pymongo import MongoClient
+import sys
+import traceback
+import random
 
-name = "old_trivia.py"
-perm = Permissions()
-sb = Scoreboard()
-client = pymongo.MongoClient()
-trivia_db = client.trivia_db
-status = trivia_db.status
-questions = trivia_db.questions
-post_id = 0
+perms = Permissions()
+tp = TempPickle()
+gc = GradiusCoins()
 
+## Name of the module, to be returned in desc()
+name = "trivia.py"
+tmp_file = 'plugins/data/tmp/trivia.tmp'
+
+
+## This is ran at the initial import of the plugin.
+## send_message_function is a callback function to send a message back to the IRC channel
 def buildup(send_message_callback):
-    print "This function is run at buildup of function."
+    print "Trivia plugin running."
     global send_message_function
+    global trivia_db
     send_message_function = send_message_callback
-    s_post = {"asked": False, "loop": False, "loop_count": 0, "answer": "", "question": ""}
-    global post_id
-    status.remove()
-    post_id = status.insert(s_post)
+
+    trivia_tmp = tp.load(tmp_file)
+    trivia_tmp['asked'] = False
+    tp.save(trivia_tmp, tmp_file)
+
+    user = 'user'
+    passwd = 'password123'
+    addr = 'localhost'
+    port = '27017'
+
+    try:
+        uri = "mongodb://" + user + ":" + passwd + "@" + addr + ":" + str(port) + "/trivia"
+        mongo_client = MongoClient(uri)
+        trivia_db = mongo_client.trivia
+
+    except:
+        print "Failed to connect to database: " + str(sys.exc_info()[:2]) + traceback.format_exc()
 
 
+## This function is called any time the bot receives input
 def send_input(inp, sender, channel):
-    if inp == "trivia" and not status.find_one()["asked"] and perm.isMod(sender):
-        trivia_question(channel)
+    trivia_tmp = tp.load(tmp_file)
+    isplit = inp.split()
 
-    if status.find_one()["asked"]:
-        trivia_answer(channel, inp, sender)
+    if perms.has_tag(sender, 'trivia') and isplit[0] == '!trivia' and len(isplit) == 3:
+        trivia_tmp['num_questions'] = int(isplit[1])
+        trivia_tmp['topic'] = isplit[2]
+        tp.save(trivia_tmp, tmp_file)
+        ask_trivia(channel, isplit[2])
 
-    if len(inp.split()) == 2 and inp.split()[0] == "trivia" and perm.isMod(sender):
-        send_message_function(channel,"Starting a trivia loop of " + inp.split()[1] + " questions.")
-        status.update({'_id': post_id}, {"$set": {"loop": True}})
-        status.update({'_id': post_id}, {"$set": {"loop_count": str(int(inp.split()[1]) - 1)}})
-        trivia_question(channel)
+    if trivia_tmp['asked']:
+        if inp.lower() == trivia_tmp['answer']:
+            trivia_tmp['num_questions'] -= 1
+            send_message_function(channel, "That is correct, " + sender + "!")
+            gc.give_coins(sender, 10)
+            trivia_tmp['asked'] = False
+            tp.save(trivia_tmp, tmp_file)
+
+            if trivia_tmp['num_questions'] > 0:
+                ask_trivia(channel, trivia_tmp['topic'])
 
 
 def execute(args_list, channel):
-    send_message_function(channel, "I have nothing to execute.")
+    send_message_function(channel, "Executing command.")
 
 
+## Returns a description of the module including the name at the top
 def desc():
-    return ""
+    return name + ": is an example plugin."
 
 
 def teardown():
-    print "Removing the Trivia module."
+    print "Removing Trivia plugin"
 
 
-def trivia_question(channel):
-    if not status.find_one()["asked"]:
-        try:
-            ind = random.randint(0, questions.count())
-            answer = str(questions.find()[ind]["answer"])
-            question = str(questions.find()[ind]["question"])
-            status.update({'_id': post_id}, {"$set": {"answer": answer}})
-            status.update({'_id': post_id}, {"$set": {"question": question}})
-            status.update({'_id': post_id}, {"$set": {"asked": True}})
-        except:
-            print "Error generating question:", sys.exc_info()
+def ask_trivia(channel, topic):
+    topics = {'lol': trivia_db.lol, 'stream': trivia_db.stream}
 
-        send_message_function(channel, str(status.find_one()["question"]))
+    if topic not in topics.keys() or topic == 'all':
+        topic = random.choice(topics.keys())
 
+    trivia_tmp = tp.load(tmp_file)
+    trivia_tmp['asked'] = True
 
-def trivia_answer(channel, message, sender):
-    if str.lower(message) == str.lower(str(status.find_one()["answer"])):
-        status.update({'_id': post_id}, {"$set": {"asked": False}})
-        send_message_function(channel,"You are correct " + sender + "!")
-        sb.addPoints(sender, 10)
-
-        if status.find_one()["loop"] and status.find_one()["loop_count"] == 0:
-            status.update({'_id': post_id}, {"$set": {"loop": False}})
-            send_message_function(channel,"I'm done with my trivia spree now.")
-
-        if status.find_one()["loop"] and status.find_one()["loop_count"] != 0:
-            status.update({'_id': post_id}, {"$set": {"loop_count": int(status.find_one()["loop_count"]) - 1}})
-            trivia_question(channel)
+    ind = random.randint(0, topics[topic].count()-1)
+    answer = str(topics[topic].find()[ind]["answer"])
+    question = str(topics[topic].find()[ind]["question"])
+    trivia_tmp['answer'] = answer.lower()
+    tp.save(trivia_tmp, tmp_file)
+    print "[DEBUG]: Answer is - " + repr(answer)
+    send_message_function(channel, question)
---
+++ plugins/gradiuscoin/README.md
@@ -0,0 +1,42 @@
+# gradiuscoin plugin
+
+## What is a gradiuscoin?
+A gradiuscoin is currency you can earn by participating in chat.  This is called mining.  During specific times, you might earn more gradiuscoins than normal (ex: during an active stream)
+
+## Commands
+
+### implemented
+N/A
+
+### future
+!buy [item]
+
+Spend your gradiuscoins on this specific item.  An item/price list can be found at the bottom of this readme.
+
+!inventory
+
+Get a pastebin link to your inventory.
+
+!wallet
+
+Show how many gradiuscoins you have.
+
+!give [target] [amount]
+
+Give the [target] [amount] gradiuscoins from your wallet.
+
+!settings
+
+Show the current gradiuscoin settings.  This will let you know if the gradiuscoin mining rate is higher than normal.
+
+!roll [amount]
+
+Bet [amount] on a roll from 1 - 100.  You earn a specific amount based on how high you roll.
+
+!help
+
+Link to this readme.
+
+## Shop
+### item : price - description
+test: 1 gc - this is a test item
